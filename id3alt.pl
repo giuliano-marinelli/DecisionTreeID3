44871ff7ded0f210eee736b8cdfbce13796fbcb5
Martinnqn <bermudez_martin.nqn@hotmail.com>
2018-06-09 14:43:42 -0300

    lee bien el archivo de entrada y esta calculando bien la entropia del primer paso, queda formar el arbol y ver si calcula bien las entropias de cada nodo intermedio

--- id3alt.pl
+++ id3alt.pl
@@ -1,5 +1,9 @@
-:- module(id3alt,[]).
+:- module(id3alt,[main/1]).
+:- use_module(library(readutil)).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%% CONJUNTOS DE PRUEBA %%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 rows_instancias([
 row(young,false,false,fair,no),
 row(young,false,false,good,no),
@@ -34,44 +38,91 @@ row(credit_raiting),
 row(class)
 ]).
 
-init() :-
-	retractall(atributo(_)),
-	retractall(dominio(_,_)),
-	retractall(instancia(_)),
-	retractall(valor(_,_,_)),
-	rows_atributos(RAtribs),
-	rows_dominios(RDomins),
-	rows_instancias(RInstas),
-	assert_atribs(RAtribs,RDomins),
-	assert_instas(1,RInstas,RAtribs).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%% FIN CONJUNTOS DE PRUEBA %%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-assert_atribs([],[]).
-assert_atribs([RAtrib|RAtribs],[RDomin|RDomins]) :-
-	RAtrib =.. [row|[Atrib]],
-	RDomin =.. [row|Domins],
-	assert(atributo(Atrib)),
-	assert_domins(Domins,Atrib),
-	assert_atribs(RAtribs,RDomins).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%% LECTURA DEL ARCHIVO DE ENTRADA %%%%%%
+%%%% Y CREACION DE PREDICADOS DE LA BD %%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+load_input(_T):-open('ejemplo_teoria.arff',read,_G,[alias(bufferEntrada)]),
+	read_string(bufferEntrada, "\n", "\r", End, String),
+	split_string(String," ","",LString),
+	agregar_predicados(LString,End).
+
+agregar_predicados(_String,-1). %alcanzo el final del archivo.
+agregar_predicados(["%"|_RestoLinea],_End):-
+	read_string(bufferEntrada, "\n", "\r", End, String),
+	split_string(String," ","",LString),
+	agregar_predicados(LString,End).
+
+agregar_predicados(["@relation",NomDataSet],_End):-
+	atom_string(NomDataSetAtom,NomDataSet),
+	assert(nombre_dataset(NomDataSetAtom)),
+	read_string(bufferEntrada, "\n", "\r", End, String),
+	split_string(String," ","",LString),
+	agregar_predicados(LString,End).
+
+agregar_predicados(["@attribute",Nombre|Valores],_End):-
+	split_string(Nombre, ",", "'", [Nombre2]),
+	atom_string(NombreAtom,Nombre2),
+	assert(atributo(NombreAtom)),
+	atomic_list_concat(Valores,List2),
+	atom_string(List2,List3),
+	split_string(List3, ",", "{'}", ListDomin),
+	assert_domins(ListDomin,NombreAtom),
+	read_string(bufferEntrada, "\n", "\r", End, String),
+	split_string(String," ","",LString),
+	agregar_predicados(LString,End).
+
+agregar_predicados(["@data"|_RestoLinea],_End):-
+	read_string(bufferEntrada, "\n", "\r", End, String),
+	split_string(String," ","",LString),
+	agregar_predicados(LString,End).
+
+agregar_predicados([Inst],_End):-
+	split_string(Inst, ",", "'", LInst),
+	listar_instancias(0,ListInsts),
+	atributos(Atrs),
+	assert_instas(1,[LInst|ListInsts],Atrs).
+
+
+listar_instancias(-1,[]).
+listar_instancias(_End,[LInst|ListInsts]):-
+	read_string(bufferEntrada, "\n", "\r", End, String),
+	split_string(String, ",", "'", LInst),
+	listar_instancias(End,ListInsts).
 
 assert_domins([],_).
 assert_domins([Domin|Domins],Atrib) :-
-	assert(dominio(Atrib,Domin)),
+	atom_string(DominAtom,Domin),
+	assert(dominio(Atrib,DominAtom)),
 	assert_domins(Domins,Atrib).
 
 assert_instas(_,[],_).
 assert_instas(Indice,[RInsta|RInstas],RAtribs) :-
-	RInsta =.. [row|Insta],
-	assert_valors(Indice,Insta,RAtribs),
+	assert_valors(Indice,RInsta,RAtribs),
 	assert(instancia(Indice)),
 	IndiceSig is Indice + 1,
 	assert_instas(IndiceSig,RInstas,RAtribs).
 	
 assert_valors(_,[],[]).
-assert_valors(Indice,[Val|Vals],[RAtrib|RAtribs]) :-
-	RAtrib =.. [row|[Atrib]],
-	assert(valor(Indice,Atrib,Val)),
+assert_valors(Indice,[Val|Vals],[Atrib|RAtribs]) :-
+	atom_string(ValAtom,Val),
+	assert(valor(Indice,Atrib,ValAtom)),
 	assert_valors(Indice,Vals,RAtribs).
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%% FIN LECTURA DEL ARCHIVO DE ENTRADA %%%%%%
+%%%%%% Y CREACION DE PREDICADOS DE LA BD %%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%% CONSULTAS A LA BD %%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 atributos(As) :- 
 	findall(A,atributo(A),As),!.
 
@@ -85,7 +136,7 @@ instancias(Is) :-
 	findall(I,instancia(I),Is),!.
 
 valores(Vs) :- 
-	findall((I,A,D),instancia(I,A,D),Vs),!.
+	findall((I,A,D),valor(I,A,D),Vs),!.
 
 clases(Cs) :-
 	findall(C,dominio(class,C),Cs),!.
@@ -99,8 +150,17 @@ instancias_por_clase(Is,Atrib,Domin,Clase) :-
 instancias_por_atrib(Is,Atrib,Domin) :-
 	findall(I,(instancia(I),valor(I,Atrib,Domin)),Is),!.
 
-run() :-
-	init(),
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%% FIN CONSULTAS A LA BD %%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%% MAIN %%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%
+
+main(T) :-
+	load_input(T),
+	%listing,
 	atributos(As),
 	dominios(Ds),
 	instancias(Is),
@@ -109,7 +169,7 @@ run() :-
 	entropia_atrib(age,EA),
 	entropia_atrib(has_job,EHJ),
 	entropia_atrib(own_house,EOH),
-	entropia_atrib(credit_raiting,ECR),
+	entropia_atrib(credit_rating,ECR),
 	write('Atributos:'),nl,
 	write(As),nl,nl,
 	write('Dominios:'),nl,
@@ -129,6 +189,13 @@ run() :-
 	write('Entropia de atributo credit_raiting:'),nl,
 	write(ECR).
 
+%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%% FIN MAIN %%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%% CALCULO DE ENTROPIA %%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 entropia(Entropia) :-
 	instancias(Is),
 	length(Is,Total),
@@ -149,6 +216,7 @@ entropia_atrib(Atrib,Entropia) :-
 	instancias(Is),
 	length(Is,Total),
 	dominios(Ds,Atrib),
+	display([Ds,Atrib]),
 	entropia_atrib1(Ds,Atrib,Total,0,Entropia).
 	
 entropia_atrib1([],_,_,Res,Res).
@@ -183,3 +251,7 @@ log2(A,Res) :-
 	log10(A,R),
 	log10(2,T),
 	Res is R/T.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%% FIN CALCULO DE ENTROPIA %%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
