b753a7f9fbe0abb28f147e366265893537ce9ca5
Martinnqn <bermudez_martin.nqn@hotmail.com>
2018-06-15 18:22:56 -0300

    agregado calculo de la matriz de confusion

--- id3.pl
+++ id3.pl
@@ -1,4 +1,4 @@
-:- module(id3,[start/0]).
+:- module(id3,[main/0]).
 :- use_module(library(readutil)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -137,14 +137,12 @@ filtrar(Instas1,[(Atrib,Valor)|Filtros], Instas) :-
 % PROGRAMA PRINCIPAL %
 %%%%%%%%%%%%%%%%%%%%%%
 
-start :-
+main :-
 	write('Calculando arbol...'),nl,nl,
 	cargar_fuente,
 	generar_arbol([],_),
 	imprimir_arbol,nl,
-	verificar_arbol(Prop),
-	Acierto is Prop*100,
-	write('Porcentaje de acierto: '),write(Acierto),write('%'),nl.
+	verificar_arbol.
 
 %%%%%%%%%%%%%%%%%%%%%%%%
 % CALCULOS DE ENTROPIA %
@@ -300,38 +298,115 @@ imprimir_nodos([(Nodo1,Domin,Nodo2,Clase)|Arcos],Indice) :-
 	imprimir_nodos(Arcos,IndiceSig).
 
 imprimir_nodos([(Nodo1,Domin,Nodo2,_C)|Arcos],Indice) :-
-	write('"'),write(Nodo1),write('"'),write(' -> '),write('"'),write(Nodo2),write('"'),write(' [label='),write(Domin),write(']'),write(';'),nl,
+	write('"'),write(Nodo1),write('"'),write(' -> '),write('"'),write(Nodo2),write(Indice),write('"'),write(' [label='),write(Domin),write(']'),write(';'),nl,
+	%IndiceSig is Indice + 1,
 	imprimir_nodos(Arcos,Indice).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % EJECUTAR CONJUNTO DE PRUEBA %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-verificar_arbol(Acierto) :-
+% toma el conjunto de datos para testear el arbol de decision, y prueba todas las instancias de testing.
+verificar_arbol:-
 	findall(Insta,instancia_test(Insta),Instas),
-	length(Instas,Total),
-	verificar_arbol_aux(Instas,Suma),
-	Acierto is Suma / Total.
+	cargar_predicados_necesarios,
+	verificar_arbol_aux(Instas), 
+	calcular_precision_recall.
 
-verificar_arbol_aux([],0).
+verificar_arbol_aux([]).
 
-verificar_arbol_aux([Insta|Instas],Suma) :-
+verificar_arbol_aux([Insta|Instas]) :-
 	raiz(Raiz),
-	verificar_instancia(Insta,Raiz,ResultInsta),
-	verificar_arbol_aux(Instas,ResultInstas),
-	Suma is ResultInsta + ResultInstas.
+	verificar_instancia(Insta,Raiz),
+	verificar_arbol_aux(Instas).
 
-verificar_instancia(Insta,Nodo,ResultInsta) :-
+verificar_instancia(Insta,Nodo) :-
 	valor(Insta,Nodo,Domin),
 	arco(Nodo,Domin,ProxNodo,_),
 	atributo(ProxNodo),
-	verificar_instancia(Insta,ProxNodo,ResultInsta).
+	verificar_instancia(Insta,ProxNodo).
 
-verificar_instancia(Insta,Nodo,ResultInsta) :-
+verificar_instancia(Insta,Nodo) :-
 	valor(Insta,Nodo,Domin),
-	arco(Nodo,Domin,Clase,_),
-	dominio(class,Clase),
-	valor(Insta,class,Clase),
-	ResultInsta is 1.
+	arco(Nodo,Domin,ClaseObtenida,_), %clasificacion segun el arbol
+	valor(Insta,class,ClaseReal), %clase Real de la instancia
+	agregar_matriz_confusion(ClaseObtenida,ClaseReal).
 
-verificar_instancia(_Insta,_Nodo,0).
+%si entra en este caso es porque la ClaseObtenida es igual a la ClaseReal. Si la clase es la clase positiva, agrega 1 a TP y termina. 
+agregar_matriz_confusion(ClaseReal,ClaseReal):-
+	clase_positiva(ClaseReal),
+	matriz_confusion(ClaseReal,TP,FN),
+	TP2 is TP+1,
+	retract(matriz_confusion(ClaseReal,_,_)),
+	assert(matriz_confusion(ClaseReal,TP2,FN)).
+
+% si entra en este caso es porque la ClaseObtenida es igual a la ClaseReal. Como llega a este predicado entonces es porque fallo en el anterior por lo que
+% la clase es la clase negativa, agrega 1 a TN y termina. 
+agregar_matriz_confusion(ClaseReal,ClaseReal):-
+	matriz_confusion(negativa,FP,TN),
+	TN2 is TN+1,
+	retract(matriz_confusion(negativa,_,_)),
+	assert(matriz_confusion(negativa,FP,TN2)).
+
+% si fallan los otros dos predicados, las clases no son iguales, por lo que la prediccion del arbol fallo. Este predicado contempla el caso donde
+% es la clase positiva, por lo que suma uno a FN.
+agregar_matriz_confusion(_ClaseObtenida,ClaseReal):-
+	clase_positiva(ClaseReal),
+	matriz_confusion(ClaseReal,TP,FN),
+	FN2 is FN+1,
+	retract(matriz_confusion(ClaseReal,_,_)),
+	assert(matriz_confusion(ClaseReal,TP,FN2)).
+
+% si alcanza este predicado es porque no son la misma clase, y la ClaseReal no es la clase positiva. Suma 1 a FP.
+agregar_matriz_confusion(_ClaseObtenida,_ClaseReal):-
+	matriz_confusion(negativa,FP,TN),
+	FP2 is FP+1,
+	retract(matriz_confusion(negativa,_,_)),
+	assert(matriz_confusion(negativa,FP2,TN)).
+
+cargar_predicados_necesarios:-
+	dominio(class,Clase), %obtiene una clase del dominio, y la considera clase positiva. El resto de las clases son negativas.
+	assert(clase_positiva(Clase)), %setea la clase positiva para que pueda cargarse la matriz_confusion.
+	assert(matriz_confusion(Clase,0,0)),
+	assert(matriz_confusion(negativa,0,0)).
+
+
+calcular_precision_recall:-
+		findall((Clas,V1,V2),matriz_confusion(Clas,V1,V2),MatrizConf),
+		write('Matriz de confusion: '),nl,
+		write(MatrizConf),nl,
+		precision(MatrizConf,Prec),
+		recall(MatrizConf,Rec),
+		total_aciertos(MatrizConf,Total),
+		write('La presicion del modelo es: '),write(Prec),nl,
+		write('El recall del modelo es: '),write(Rec),nl,
+		Total2 is Total*100,
+		write('El total de aciertos es: '),write(Total2),write('%'),nl.
+
+precision([(negativa,FP,_TN),(_ClasePositiva,TP,_FN)],Prec):-
+	TP+FP > 0,
+	Prec is TP/(TP+FP).
+
+precision([(_ClasePositiva,TP,_FN),(negativa,FP,_TN)],Prec):- 
+	TP+FP > 0,
+	Prec is TP/(TP+FP).
+
+%si llega a este caso es porque habia una division por cero. Si llego hasta aca, es porque no hizo nada bien..
+precision(_Matriz,0). 
+
+recall([(negativa,_FP,_TN),(_ClasePositiva,TP,FN)],Rec):-
+	TP+FN > 0,
+	Rec is TP/(TP+FN).
+
+recall([(_ClasePositiva,TP,FN),(negativa,_FP,_TN)],Rec):- 
+	TP+FN > 0,
+	Rec is TP/(TP+FN).
+
+%si llega a este caso es porque habia una division por cero. Si llego hasta aca, es porque no hizo nada bien..
+recall(_Matriz,0). 
+
+total_aciertos([(negativa,FP,TN),(_ClasePositiva,TP,FN)],Total):-
+	Total is (TP+TN)/(FP+FN+TP+TN).
+
+total_aciertos([(_ClasePositiva,TP,FN),(negativa,FP,TN)],Total):-
+	Total is (TP+TN)/(FP+FN+TP+TN).
